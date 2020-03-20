ff55f2c38350869ff715945ec71a5d37c3d344be
obumneme20 <obumdarlington20@gmail.com>
2020-03-20 05:42:52 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function btnClick(){
+
+    var str = document.getElementById("first_name").value;
+   
+    if(first_name == ""){
+
+        alert("please fill the field")
+
+    }else{
+
+        var count = str.length;
+        document.getElementById("screen").value = count;
+    
+    }
+    
+}  
+
+
+
+
+
+
+
+
+
